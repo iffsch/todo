8bbaf8e452be04399fae39fc10b3f1fd7f4fa389
SchnJulian <julian-schnabel@posteo.de>
2020-04-13 18:35:40 +0200

    Initial Commit

---
+++ todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo
+//
+//  Created by Julian Schnabel on 13.04.20.
+//  Copyright © 2020 Julian Schnabel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
